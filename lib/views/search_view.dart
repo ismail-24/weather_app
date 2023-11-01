c30209e12e218056af5f0689ccdceb63478e27e2
ismail-24 <ismailtarek888@gmail.com>
2023-11-01 16:34:04 +0200

    Update search_view.dart

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -2,9 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
-import 'package:weather_app/models/weather_model.dart';
 import 'package:weather_app/services/weather_service.dart';
-import 'package:weather_app/widgets/weather_info_body.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({Key? key}) : super(key: key);
@@ -20,8 +18,7 @@ class SearchView extends StatelessWidget {
         child: Center(
           child: TextField(
             onSubmitted: (value) async {
-              WeatherModel weatherModel = await WeatherService(Dio())
-                  .getCurrentWeather(cityName: value);
+              await WeatherService(Dio()).getCurrentWeather(cityName: value);
               var getWeatherCubit = BlocProvider.of<GetWeatherCubit>(context);
               getWeatherCubit.getWeather(cityName: value);
               Navigator.pop(context);
